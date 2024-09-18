dac89fed1410b5c1c34bd248a8edb6f5c559b60a
hanyoung <phanyoung@naver.com>
2024-09-18 16:47:17 +0900

    config: google search console 활성화

--- site.config.js
+++ site.config.js
@@ -42,7 +42,7 @@ const CONFIG = {
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
