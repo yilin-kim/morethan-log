2ac3a8f61d0f8fe10aa5911b1bd2b29d3b4d0c16
hanyoung <phanyoung@naver.com>
2024-09-26 14:56:01 +0900

    design: avatar 이미지 그림자 있는 형태로 변경

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Hanyoung(Ryan)",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.webp", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "Live without regrets.",
     email: "phanyoung@naver.com",
